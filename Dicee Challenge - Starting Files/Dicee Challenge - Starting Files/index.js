50fa59555ce636102d3126cd652a83b622b340ab
Jisha-Maniamma <jishapillai1991@yahoo.co.in>
2021-09-21 07:36:55 +0900

    Dice Game

--- Dicee Challenge - Starting Files/Dicee Challenge - Starting Files/index.js
+++ Dicee Challenge - Starting Files/Dicee Challenge - Starting Files/index.js
@@ -3,4 +3,5 @@ var RandomNumber1=Math.floor(Math.random()*6)+1;
 
 var RandomNumber2=Math.floor(Math.random()*6)+1;
 
-Image1=
+Image1="images/dice"+RandomNumber1+".png";
+Image2="images/dice"+RandomNumber2+".png";
