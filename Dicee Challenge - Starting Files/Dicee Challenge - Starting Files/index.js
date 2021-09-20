13fc6217009d406d528e11cfbc751d107d439a46
Jisha-Maniamma <jishapillai1991@yahoo.co.in>
2021-09-21 07:44:51 +0900

    Dice game

--- Dicee Challenge - Starting Files/Dicee Challenge - Starting Files/index.js
+++ Dicee Challenge - Starting Files/Dicee Challenge - Starting Files/index.js
@@ -5,3 +5,18 @@ var RandomNumber2=Math.floor(Math.random()*6)+1;
 
 Image1="images/dice"+RandomNumber1+".png";
 Image2="images/dice"+RandomNumber2+".png";
+
+
+document.querySelectorAll("img")[0].setAttribute("src",Image1);
+
+document.querySelectorAll("img")[1].setAttribute("src",Image2);
+
+
+if(RandomNumber1>RandomNumber2){
+  document.querySelector("h1").innerHTML("Player1 Won");
+}
+else if (RandomNumber1<RandomNumber) {
+    document.querySelector("h1").innerHTML("Player2 Won");
+}else{
+    document.querySelector("h1").innerHTML("Its a draw!");
+}
