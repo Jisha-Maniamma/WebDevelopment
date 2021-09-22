5daa73a6454359cb8e10759f536f96fd36c48d46
Jisha-Maniamma <jishapillai1991@yahoo.co.in>
2021-09-22 15:28:33 +0900

    Drum Kit

--- Drum Kit Starting Files/Drum Kit Starting Files/index.js
+++ Drum Kit Starting Files/Drum Kit Starting Files/index.js
@@ -1,45 +1,54 @@
 var a = document.querySelectorAll("button");
 
+document.addEventListener("keypress",function(e){
+     playSound(e.key);
+});
+
 for (var i = 0; i <= a.length; i++) {
   a[i].addEventListener("click", function() {
     // var audio=new Audio("sounds/tom-3.mp3");
     // audio.play();
     // this.style.color="white";
     var audio;
-    switch (this.innerHTML) {
-      case "w":
-        audio = new Audio("sounds/tom-1.mp3");
+    playSound(this.innerHTML);
 
-        break;
-      case "a":
-        audio = new Audio("sounds/tom-2.mp3");
-
-        break;
-      case "s":
-        audio = new Audio("sounds/tom-3.mp3");
-
-        break;
-      case "d":
-        audio = new Audio("sounds/tom-4.mp3");
-
-        break;
-      case "j":
-        audio = new Audio("sounds/snare.mp3");
-
-        break;
-      case "k":
-        audio = new Audio("sounds/crash.mp3");
-
-        break;
-      case "l":
-        audio = new Audio("sounds/kick-bass.mp3");
-
-        break;
-
-      default:
-        alert("what!");
-
-    }
-    audio.play();
   });
+}
+
+function playSound(key){
+  switch (key) {
+    case "w":
+      audio = new Audio("sounds/tom-1.mp3");
+
+      break;
+    case "a":
+      audio = new Audio("sounds/tom-2.mp3");
+
+      break;
+    case "s":
+      audio = new Audio("sounds/tom-3.mp3");
+
+      break;
+    case "d":
+      audio = new Audio("sounds/tom-4.mp3");
+
+      break;
+    case "j":
+      audio = new Audio("sounds/snare.mp3");
+
+      break;
+    case "k":
+      audio = new Audio("sounds/crash.mp3");
+
+      break;
+    case "l":
+      audio = new Audio("sounds/kick-bass.mp3");
+
+      break;
+
+    default:
+      alert("what its not the key that will play this!");
+
+  }
+  audio.play();
 }
