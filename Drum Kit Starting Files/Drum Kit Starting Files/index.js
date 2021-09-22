d242e9c41a02077d6421dd82247bac5e732a7d16
Jisha-Maniamma <jishapillai1991@yahoo.co.in>
2021-09-22 14:23:00 +0900

    Drum Kit

--- Drum Kit Starting Files/Drum Kit Starting Files/index.js
+++ Drum Kit Starting Files/Drum Kit Starting Files/index.js
@@ -3,8 +3,8 @@ var a=document.querySelectorAll("button");
 
 for(var i=0;i<=a.length;i++){
   a[i].addEventListener("click", function(){
-  var audio=new Audio("sounds/tom-3.mp3");
-  audio.play();
-
+  // var audio=new Audio("sounds/tom-3.mp3");
+  // audio.play();
+this.style.color="white";
   });
 }
