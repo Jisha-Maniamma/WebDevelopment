4443ba5c3e4e65d460e7f16f4e71ed7c3b1ca243
Jisha-Maniamma <jishapillai1991@yahoo.co.in>
2021-10-06 09:25:56 +0900

    Express-Server/NodeJs/Nodemon

--- my-express-server/server.js
+++ my-express-server/server.js
@@ -3,8 +3,17 @@ const app=express();
 
 
 app.get("/",function(request,response){
-response.send("hello");
-})
+response.send("<h1>hello, Jisha</h1>");
+});
+app.get("/contact",function(request,response){
+  response.send("<h1>contact me at Juntos.com</h1>");
+});
+app.get("/about",function(request,response){
+  response.send("<p> hai thi sis Jisha Maniamma working in Japan. My home country is india. I love India and Japan.</p>");
+});
+app.get("/hobbies",function(request,response){
+  response.send("<p> Painiting.</p>");
+});
 app.listen(3000,function(){
   console.log("server started on port 3000");
 });
